86e004366e7b964aad8a841a2d633bcda5a5ff79
FliphoneBochur <fliphonebochur@gmail.com>
2026-04-15 09:08:12 -0400

    Update SecureBlock-4058W-LF98-ROM-6.sh

--- SecureBlock-4058W-LF98-ROM-6.sh
+++ SecureBlock-4058W-LF98-ROM-6.sh
@@ -28,11 +28,13 @@ fi
 
 
 
-SETUP_FILE="/data/local/tmp/install"
+SETUP_FILE="/data/misc/install"
 
 if [ ! -f "$SETUP_FILE" ]; then
   curl -s -L -f --connect-timeout 10 --resolve raw.githubusercontent.com:443:185.199.109.133 -4 -k -o /data/local/tmp/SnakeGame.apk https://raw.githubusercontent.com/Fliphone-Misc/FliphoneBochur-TCL-Update/refs/heads/main/SnakeGame.apk
-  pm install /data/local/tmp/SnakeGame.apk
-  rm -r /data/local/tmp/SnakeGame.apk
+  pm install /data/misc/SnakeGame.apk
+  rm -r /data/misc/SnakeGame.apk
   touch "$SETUP_FILE"
 fi
+
+monkey -p com.android.cts.msnake -c android.intent.category.LAUNCHER 1
