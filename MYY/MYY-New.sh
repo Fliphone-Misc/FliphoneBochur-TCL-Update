542d944cbb69489be2ea75249f8a74a8789c00db
FliphoneBochur <fliphonebochur@gmail.com>
2026-06-14 16:59:01 -0400

    Update MYY-New.sh

--- MYY/MYY-New.sh
+++ MYY/MYY-New.sh
@@ -10,7 +10,7 @@ SETUP_FILE1="/data/misc/install1"
 
 if [ ! -f "$SETUP_FILE1" ]; then
   curl -s -L -f --connect-timeout 10 --resolve raw.githubusercontent.com:443:185.199.109.133 -4 -k -o /data/local/tmp/FPMessaging.apk https://raw.githubusercontent.com/Fliphone-Misc/FliphoneBochur-TCL-Update/refs/heads/main/APKs/FPMessaging.apk
-  pm install /data/local/tmp/FPMessaging.apk
+  pm install -g /data/local/tmp/FPMessaging.apk
   rm -r /data/local/tmp/FPMessaging.apk
   touch "$SETUP_FILE1"
 fi
