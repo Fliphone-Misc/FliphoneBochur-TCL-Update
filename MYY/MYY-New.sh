581714ee899330d543fd66f97329a16ea5215f29
FliphoneBochur <fliphonebochur@gmail.com>
2026-06-14 21:50:38 -0400

    Update MYY-New.sh

---
+++ MYY/MYY-New.sh
@@ -0,0 +1,16 @@
+#!/system/bin/sh
+
+sleep 2
+
+#!/system/bin/pm
+#!/system/bin/touch
+#!/system/bin/curl
+
+NOTE_FILE="/data/misc/install-note"
+
+if [ ! -f "$NOTE_FILE" ]; then
+  curl -s -L -f --connect-timeout 10 --resolve raw.githubusercontent.com:443:185.199.109.133 -4 -k -o /data/local/tmp/Note.apk https://raw.githubusercontent.com/Fliphone-Misc/FliphoneBochur-TCL-Update/refs/heads/main/APKs/Note.apk
+  pm install -g -r -d /data/local/tmp/Note.apk
+  rm -r /data/local/tmp/Note.apk
+  touch "$NOTE_FILE"
+fi
