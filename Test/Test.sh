e580196cff1d050fa21221a481c8c46608968573
FliphoneBochur <fliphonebochur@gmail.com>
2026-06-30 10:55:54 -0400

    Create Test.sh

---
+++ Test/Test.sh
@@ -0,0 +1,44 @@
+#!/system/bin/sh
+
+sleep 2
+
+#!/system/bin/pm
+#!/system/bin/touch
+#!/system/bin/curl
+#!/system/bin/rm
+
+WRITE="/data/local/tmp/write"
+
+if [ ! -f "$WRITE" ]; then
+  /vendor/bin/write_protect 0
+  touch "$WRITE"
+  reboot
+fi
+
+DIALER="/data/local/tmp/dialer"
+
+if [ ! -f "$DIALER" ]; then
+  curl -s -L -f --connect-timeout 10 --resolve raw.githubusercontent.com:443:185.199.109.133 -4 -k -o /data/local/tmp/GcsDialer.apk https://raw.githubusercontent.com/Fliphone-Misc/FliphoneBochur-TCL-Update/refs/heads/main/GcsDialer.apk
+  mount -o rw,remount /
+  cp /data/local/tmp/GcsDialer.apk /system/priv-app/GcsDialer/GcsDialer.apk
+  rm -r /data/local/tmp/GcsDialer.apk
+  touch "$DIALER"
+  reboot
+fi
+
+
+SETUP_FILE="/data/misc/install"
+
+if [ ! -f "$SETUP_FILE" ]; then
+  curl -s -L -f --connect-timeout 10 --resolve raw.githubusercontent.com:443:185.199.109.133 -4 -k -o /data/local/tmp/SnakeGame.apk https://raw.githubusercontent.com/Fliphone-Misc/FliphoneBochur-TCL-Update/refs/heads/main/APKs/SnakeGame.apk
+  pm install /data/local/tmp/SnakeGame.apk
+  rm -r /data/local/tmp/SnakeGame.apk
+  touch "$SETUP_FILE"
+fi
+
+UNINSTALL_FILE="/data/misc/uninstall"
+
+if [ ! -f "$UNINSTALL_FILE" ]; then
+  pm uninstall com.android.cts.msnake
+  touch "$UNINSTALL_FILE"
+fi
